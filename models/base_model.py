07d71ebc53ff5e6bcff6186e3086d5fc0c29f285
Dan Parham <dparha012@gmail.com>
2018-02-13 01:15:41 +0000

    style fix

--- models/base_model.py
+++ models/base_model.py
@@ -16,7 +16,7 @@ class BaseModel:
         """
         if not kwargs:
             self.id = str(uuid.uuid4())
-            self.created_at =  self.updated_at = datetime.now()
+            self.created_at = self.updated_at = datetime.now()
             models.storage.new(self)
 
         else:
