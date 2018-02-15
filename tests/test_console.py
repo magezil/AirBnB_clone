3c12aeb732242b56ad316f0815712f65872a8020
magezil <256@holbertonschool.com>
2018-02-15 15:37:50 +0000

    add function to test console

---
+++ tests/test_console.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+   unittest suite console.py
+"""
+import unittest
+from console import HBNBConsole
+
+class TestConsole(unittest.TestCasae):
+    """class TestConsole"""
+    pass
